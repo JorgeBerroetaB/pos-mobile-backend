a112ca1381752709504690c7c7766ff4b2c7c355
JorgeBerroetaB <jorge.berroeta.1@gmail.com>
2026-04-27 21:17:30 -0400

    Init: Proyecto base de Spring Boot para POS Mobile

---
+++ src/test/java/com/yupi/pos/PosMobileBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yupi.pos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PosMobileBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
